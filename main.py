b67d472cd99281dad9e06bb2b5c2d78393c3c674
Jose Antonio Campos Segura <jacampossegura@gmail.com>
2025-04-09 12:25:53 +0200

    Update main.py
    Add DOLIAPI_KEY

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def interpretar_prompt(request: PromptRequest):
         return {"error": "Faltan variables de entorno para DOLIBARR_API_URL o DOLIBARR_API_KEY"}
 
     headers = {
-        "Authorization": f"Bearer {DOLIBARR_API_KEY}",
+        "DOLAPIKEY": DOLIBARR_API_KEY,
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
