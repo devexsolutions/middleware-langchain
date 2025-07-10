bd2a6ade1cb04acf20bd7bb5de6e77491cdf4dd3
Jose Antonio Campos Segura <jacampossegura@gmail.com>
2025-07-10 22:16:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://joseantoniocampos.pro"],  # Aquí tu frontend real
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
